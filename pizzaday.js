38541b8d714f4d75340d3b4bf2b9372426d884c9
Stanislav Sysoiev <sysstas@gmail.com>
2016-02-10 15:08:51 +0200

    User authentication and Existing user list added

--- pizzaday.js
+++ pizzaday.js
@@ -1,23 +1,25 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  /////////////////////////// Adjustment accounts-ui //////////////////////////////
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  //////////////////////////////////////////////  Helpers //////////////////////////
+  Template.userlist.helpers({
+    user:function(){      
+      return Meteor.users.find(); 
+    }    
+  }); 
+
+  //////////////////////////////// Events /////////////////////////////////////
+   
+
+
+
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
+
 }
