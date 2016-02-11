f59c163f7af6459b037904434ca959f2419641b7
sysstas <sysstas@gmail.com>
2016-02-11 15:11:07 +0200

    Google authentication added

--- pizzaday.js
+++ pizzaday.js
@@ -6,8 +6,21 @@ if (Meteor.isClient) {
 
   //////////////////////////////////////////////  Helpers //////////////////////////
   Template.userlist.helpers({
-    user:function(){      
-      return Meteor.users.find(); 
+    user:function(){ 
+      var correct = Meteor.users.find();
+      console.log("user sais: "+correct);
+      if(correct != ""){
+        return correct;
+      }
+      return  ;
+    },
+    usernameGoogle: function(){
+      var correct = this.services.google.name;
+      console.log("Google sais: "+correct);      
+      if(correct != ""){
+        return correct;
+      }      
+      return;
     }    
   }); 
 
@@ -20,6 +33,15 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  
+  // first, remove configuration entry in case service is already configured
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "416436598179-50lvops42dpr7ojef8og2i0du3ghkq7t.apps.googleusercontent.com",
+  loginStyle: "popup",
+  secret: "IF3NUy0rlZTc_R2yjp7aN4Nn"
+});  
 
 }
